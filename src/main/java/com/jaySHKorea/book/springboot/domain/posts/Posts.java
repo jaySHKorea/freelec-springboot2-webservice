a51e7d0411deb3e4213c11ec7ebaafbc201c62af
jaySHKorea <seung2586@gmail.com>
2020-10-26 03:08:23 +0900

    게시판 domain 클래스 작성

---
+++ src/main/java/com/jaySHKorea/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,39 @@
+package com.jaySHKorea.book.springboot.domain.posts;
+
+import com.jaySHKorea.book.springboot.domain.BaseTimeEntity;
+import com.sun.javafx.beans.IDProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // 기본 생성자 자동추가
+@Entity // 주요 어노테이션을 클래스에 가깝게 , 테이블과 매핑되는 클래스, JPA가 제공
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스 생성, 생성자 상단에 선언시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
