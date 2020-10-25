01b9cb05fdb1f01ba464ec41a10a8ea0576147d1
jaySHKorea <seung2586@gmail.com>
2020-10-26 03:09:57 +0900

    JPA Auditing 어노테이션 추가

--- src/main/java/com/jaySHKorea/book/springboot/Application.java
+++ src/main/java/com/jaySHKorea/book/springboot/Application.java
@@ -2,7 +2,9 @@ package com.jaySHKorea.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA AUditing 활성화
 @SpringBootApplication // 스프링 부트의 자동 설정, Bean 읽기와 생성이 모두 자동
 public class Application {
     public static void main(String[] args){
