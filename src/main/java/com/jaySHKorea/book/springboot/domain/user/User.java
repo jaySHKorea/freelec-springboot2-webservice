301aa703de5776e637fe446edb916837d313409e
jaySHKorea <seung2586@gmail.com>
2020-11-12 16:38:30 +0900

    entity 클래스 작성

---
+++ src/main/java/com/jaySHKorea/book/springboot/domain/user/User.java
@@ -0,0 +1,51 @@
+package com.jaySHKorea.book.springboot.domain.user;
+
+import com.jaySHKorea.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING) //JPA로 DB에 저장할때, enum 값을 어떤 형태로 저장할지
+                                    // 기본 설정은 int 숫자, 코드 의미 해석이 힘드므로 String 으로
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role){
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture){
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+}
