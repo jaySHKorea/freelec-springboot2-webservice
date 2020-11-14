7193e223bd58b7edb280975b12d37fb74fe1eb62
jaySHKorea <seung2586@gmail.com>
2020-11-14 16:34:18 +0900

    Resolver 인터페이스 구현 (user 세션)

---
+++ src/main/java/com/jaySHKorea/book/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,34 @@
+package com.jaySHKorea.book.springboot.config.auth;
+
+import com.jaySHKorea.book.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter){ // 컨트롤러 메서드의 특정 파라미터를 지원하는지 판단
+        // 파라미터에 @LoginUser 어노테이션이 붙어있는가
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        // 파라미터 클래스 타입이 SessionUser.class인 경우 true를 반환
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override // 파라미터에 전달할 객체 생성
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception{
+        return httpSession.getAttribute("user"); // 세션에서 객체 가져오기
+    }
+}
