1b067810f17c10804e73acdbe4e944036a205ace
jaySHKorea <seung2586@gmail.com>
2020-11-14 18:24:57 +0900

    네이버 로그인 생성자 추가

--- src/main/java/com/jaySHKorea/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jaySHKorea/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -27,15 +27,31 @@ public class OAuthAttributes {
 
     // OAuth2User의 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야함
     public static OAuthAttributes of (String registrationId, String userNameAttributeName,Map<String, Object> attributes){
+        if ("naver".equals(registrationId)){
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
+    // 구글 로그인
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    // 네이버 로그인
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String,Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
